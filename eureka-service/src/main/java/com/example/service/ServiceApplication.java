113d31885fb1ea501107b0d0a38fe457d4ef0659
lilj <hongyullj@sohu.com>
2021-01-11 20:34:29 +0800

    eurekaServer模块

--- eureka-service/src/main/java/com/example/service/ServiceApplication.java
+++ eureka-service/src/main/java/com/example/service/ServiceApplication.java
@@ -2,14 +2,14 @@ package com.example.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableConfigServer
+@EnableConfigServer
 public class ServiceApplication {
 
 	public static void main(String[] args) {
